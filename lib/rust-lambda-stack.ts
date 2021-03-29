5f4a4f6373053b36974a84be4d5e3d3cecbacc07
Ryan <dsouza.ryan.11@gmail.com>
2021-03-29 16:05:25 +0530

    initial commit

--- lib/rust-lambda-stack.ts
+++ lib/rust-lambda-stack.ts
@@ -1,9 +1,20 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import * as lambda from '@aws-cdk/aws-lambda'
 
 export class RustLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    // Function that calls Rust
+    new lambda.Function(this, 'rust-hello', {
+      code: lambda.Code.fromAsset('resources'),
+      runtime: lambda.Runtime.PROVIDED_AL2,
+      handler: 'not.required',
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        RUST_BACKTRACE: '1',
+      },
+    })
   }
 }
